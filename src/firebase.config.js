714305dbc626b62d7ce3d0f49f3beb92fe5c491d
rasel754 <raselahmed73614@gmail.com>
2024-04-16 18:21:09 +0600

    set firebase and navbar

---
+++ src/firebase.config.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBFTLiiSN-3Rni56L9CCArtgpiCigifLEM",
+  authDomain: "assignment-nine-8e58f.firebaseapp.com",
+  projectId: "assignment-nine-8e58f",
+  storageBucket: "assignment-nine-8e58f.appspot.com",
+  messagingSenderId: "1078062611819",
+  appId: "1:1078062611819:web:a88c0abbde5b989275e02e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+export default auth;
