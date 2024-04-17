3bd235fb734518057cb8b2f3cac93b5601faa248
rasel754 <raselahmed73614@gmail.com>
2024-04-17 11:13:43 +0600

    banner section done

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -1,19 +1,30 @@
-import React from "react";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import banner1 from '../../assets/banner1.jpg';
+import banner2 from '../../assets/banner2.jpg';
+import banner3 from '../../assets/banner3.jpg';
+import banner5 from '../../assets/banner5.jpg';
+import banner6 from '../../assets/banner6.jpg';
 
+import banner4 from '../../assets/banner4.avif';
 const Banner = () => {
   return (
-    <div>
-      <swiper-container
-        slides-per-view="3"
-        speed="500"
-        loop="true"
-        css-mode="true"
-      >
-        <swiper-slide>Slide 1</swiper-slide>
-        <swiper-slide>Slide 2</swiper-slide>
-        <swiper-slide>Slide 3</swiper-slide>
-      </swiper-container>
-    </div>
+
+      <Swiper
+      spaceBetween={50}
+      slidesPerView={2}
+      onSlideChange={() => console.log('slide change')}
+      onSwiper={(swiper) => console.log(swiper)}
+    >
+      <SwiperSlide><img className='w-full h-96' src={banner1} alt="" /></SwiperSlide>
+      <SwiperSlide><img className='w-full h-96' src={banner2} alt="" /></SwiperSlide>
+      <SwiperSlide><img className='w-full h-96' src={banner5} alt="" /></SwiperSlide>
+      <SwiperSlide><img className='w-full h-96' src={banner4} alt="" /></SwiperSlide>
+      <SwiperSlide><img className='w-full h-96' src={banner3} alt="" /></SwiperSlide>
+      <SwiperSlide><img className='w-full h-96' src={banner6} alt="" /></SwiperSlide>
+     
+    </Swiper>
+   
   );
 };
 
